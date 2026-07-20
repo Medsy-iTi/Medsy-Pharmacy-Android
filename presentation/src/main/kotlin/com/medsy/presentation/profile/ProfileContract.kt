b14389962e0a6ee1f7b553489979175c1f614d6c
Ahmed Mohamed <ahmedmonataf@gmail.com>
2026-07-20 20:49:39 +0300

    feat: profile contract

--- presentation/src/main/kotlin/com/medsy/presentation/profile/ProfileContract.kt
+++ presentation/src/main/kotlin/com/medsy/presentation/profile/ProfileContract.kt
@@ -2,9 +2,17 @@ package com.medsy.presentation.profile
 
 import com.medsy.domain.common.preferences.model.ThemeMode
 
+import com.medsy.domain.common.MedsyError
+import com.medsy.domain.pharmacist.model.Pharmacist
+import com.medsy.domain.pharmacy.model.MyPharmacy
+
 data class ProfileState(
     val themeMode: ThemeMode = ThemeMode.System,
     val isReceivingOrders: Boolean = true,
+    val isLoading: Boolean = false,
+    val pharmacist: Pharmacist? = null,
+    val pharmacy: MyPharmacy? = null,
+    val error: MedsyError? = null
 )
 
 sealed interface ProfileUIIntent {
