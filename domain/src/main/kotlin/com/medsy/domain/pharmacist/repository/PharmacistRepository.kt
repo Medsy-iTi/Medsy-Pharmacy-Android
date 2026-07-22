c5b0846f14fa134099d8f34c94de5782f4f4f01c
Ahmed Mohamed <ahmedmonataf@gmail.com>
2026-07-22 14:47:19 +0300

    feat:pharmacist repo

--- domain/src/main/kotlin/com/medsy/domain/pharmacist/repository/PharmacistRepository.kt
+++ domain/src/main/kotlin/com/medsy/domain/pharmacist/repository/PharmacistRepository.kt
@@ -15,4 +15,5 @@ interface PharmacistRepository {
     ): MedsyResult<Pharmacist, MedsyError>
     suspend fun removePharmacistFromPharmacy(pharmacistId: Long, pharmacyId: Long): EmptyMedsyResult<MedsyError>
     suspend fun leavePharmacy(pharmacyId: Long): EmptyMedsyResult<MedsyError>
+    suspend fun setPresence(onDuty: Boolean): MedsyResult<com.medsy.domain.pharmacist.model.PresenceStatus, MedsyError>
 }
