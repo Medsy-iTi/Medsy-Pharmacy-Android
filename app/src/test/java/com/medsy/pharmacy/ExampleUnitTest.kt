adab963d3ca6784ae40058e7c6fa23d67a6937f2
ELDemy <mahmoudeldemerdash5@gmail.com>
2026-08-14 05:16:15 +0300

    feat(build): update version code and name, add testing dependencies

--- app/src/test/java/com/medsy/pharmacy/ExampleUnitTest.kt
+++ app/src/test/java/com/medsy/pharmacy/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.example.medsypahrm
-
-import org.junit.Test
+package com.medsy.pharmacy
 
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
